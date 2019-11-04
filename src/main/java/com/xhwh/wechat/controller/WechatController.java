666a5c4495cfa332162099526d82ce9187a07580
xuxiong <18720920393@163.com>
2019-11-04 20:09:17 +0800

    1、接受消息接口开发

--- src/main/java/com/xhwh/wechat/controller/WechatController.java
+++ src/main/java/com/xhwh/wechat/controller/WechatController.java
@@ -1,11 +1,19 @@
 package com.xhwh.wechat.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import me.chanjar.weixin.mp.api.WxMpMessageRouter;
 import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.bean.message.WxMpXmlMessage;
+import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 @Slf4j
@@ -22,5 +30,22 @@ public class WechatController {
             return echostr; //接入校验合法，返回
         }
         return null;
+    }
+
+    //接受用户消息
+    @PostMapping("wechatAccess")
+    @ResponseBody
+    public void wechatAccess(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        WxMpXmlMessage wxMpXmlMessage = WxMpXmlMessage.fromXml(request.getInputStream());
+        log.info(wxMpXmlMessage.toString());
+        WxMpMessageRouter wxMpMessageRouter = new WxMpMessageRouter(wxMpService);
+        wxMpMessageRouter.rule().async(false).end();
+        WxMpXmlOutMessage wxMpXmlOutMessage = wxMpMessageRouter.route(wxMpXmlMessage);
+        if(wxMpXmlOutMessage != null){
+            log.info(wxMpXmlOutMessage.toXml());
+            response.getWriter().print(wxMpXmlOutMessage.toXml());
+        }else {
+            response.getWriter().print("");
+        }
     }
 }
